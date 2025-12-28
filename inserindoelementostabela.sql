cfc5e67744d800ca60a99de52f753e4bc538039f
Yago Ferreira <yagocmferreira9@gmail.com>
2025-12-27 22:21:18 -0300

    Create inserindoelementostabela.sql

---
+++ inserindoelementostabela.sql
@@ -0,0 +1,6 @@
+INSERT INTO Vendas(ID_venda,curso, aluno, Estado, valor)
+VALUES
+(1, 'Python Básico',	'Yago',	'SP', 299.90)
+(2, 'SQL para Iniciantes',	'Ana',	'RJ', 199.90)
+(3, 'Java Avançado', 'Carlos',	'MG', 399.90)
+(4, 'Data Science', 'Maria', 'PB', 499.90);
